af8e1240042e4ba9a2452ff644c58beea7d2abab
ThomasMurat <thomasmurat1992@gmail.com>
2020-06-05 14:58:20 +0200

    up exo5

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Php-Partie1-exo4</title>
+    </head>
+    <body>
+    <p>
+        <?php 
+            $varint = null;
+            echo $varint ;
+        ?>
+    </p>
+        <?php
+            $varint = 2;
+            echo $varint ;
+        ?>
+    </p>
+    </body>
+</html>
